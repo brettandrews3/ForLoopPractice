d9bf0afa92558675e2697bac01018d9f257ead48
Brett Andrews <brettandrews3@gmail.com>
2020-04-07 16:06:02 -0500

    First round of names works

---
+++ ForLoopPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForLoopPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentNames = new string[] { "Michael", "Anyong", "Lucille" };
+            foreach (var student in studentNames)
+            {
+                Console.WriteLine(student);
+            }
+
+            
+            }
+        }
+    }
+
