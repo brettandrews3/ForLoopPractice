4aeff4d61a0e5e25eb1ea717f517cacceb516e59
Brett Andrews <brettandrews3@gmail.com>
2020-04-07 16:37:22 -0500

    3 foreach loops working

--- ForLoopPractice/Program.cs
+++ ForLoopPractice/Program.cs
@@ -10,10 +10,38 @@ namespace ForLoopPractice
             foreach (var student in studentNames)
             {
                 Console.WriteLine(student);
+                Console.WriteLine(student);
             }
 
-            
+            var studentPeople = new string[] { "Bob Loblaw", "Barry", "Gene" };
+            foreach (var student in studentPeople)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
             }
+
+            var studentTypes = new string[] { "Buster", "Tobias", "Maebe" };
+            foreach (var student in studentTypes)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+
+            }
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("You're doing it, goddammit!");
+
+                    }
+            var studentNames = new string[] { "Brett", "David", "Michael" };
+            foreach (var student in studentNames)
+            {
+                Console.WriteLine(student);
+            }
+
         }
     }
+}
+
+    
 
